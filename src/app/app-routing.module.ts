62569e5f3cd7e3713fce126842f7abec22358f53
mohneeshParkhi <mohneesh0088@gmail.com>
2019-02-20 13:40:47 +0530

    lazy loading example successfull

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// configuring routes variable to store refrence to children modules..
+const routes: Routes = [
+  {
+    path:'customers',
+    loadChildren:'./customers/customers.module#CustomersModule'
+  },
+  {
+    path:'orders',
+    loadChildren:'./orders/orders.module#OrdersModule'
+  },
+  {
+    path: '',
+    redirectTo:'',
+    pathMatch:'full'
+  }
+];
 
+  // registering with 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
